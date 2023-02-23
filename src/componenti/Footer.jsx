f4e585269737d6579d22ea3174fb9455f4faaacf
Studente <samu.manenti@gmail.com>
2023-02-23 12:32:34 +0100

    seconda lezione

---
+++ src/componenti/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return(
+        <footer>
+            <div className='footer-container'>
+                <div>sezione 1</div>
+                <div>sezione 2</div>
+                <div>sezione 3</div>
+                <div>sezione 4</div>
+            </div>
+        </footer>
+    )
+};
+
+export default Footer;
---
+++ src/pages/ChiSono.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ChiSono = () => {
+    return(
+    <h1>CHISONO</h1>
+    )
+};
+
+export default ChiSono;
